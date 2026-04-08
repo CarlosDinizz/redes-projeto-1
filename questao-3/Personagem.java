604f101d2bd074fe8b830cd3ccdbe93fd2049f5c
Rafael Moutinho Tessarotto <94569688+RafaelTessarotto@users.noreply.github.com>
2026-04-08 15:36:04 -0300

    Add abstract Personagem class for game characters

---
+++ questao-3/Personagem.java
@@ -0,0 +1,50 @@
+package jogo.personagens;
+
+import java.util.Random;
+
+public abstract class Personagem {
+    protected String nome;
+    protected int vida;
+    protected int vidaMaxima;
+    protected int ataque;
+    private static final Random random = new Random();
+
+    public Personagem(String nome, int vida, int ataque) {
+        this.nome = nome;
+        this.vida = vida;
+        this.vidaMaxima = vida;
+        this.ataque = ataque;
+    }
+
+    public void atacar(Personagem alvo) {
+        int rolar = random.nextInt(20) + 1;
+        boolean critico = rolar > 15;
+
+        int dano = critico ? (int)(ataque * 1.3) : ataque;
+        alvo.receberDano(dano);
+
+        if (critico) {
+            System.out.println(nome + " acertou um CRÍTICO! (rolou " + rolar + ") → " + dano + " de dano em " + alvo.getNome());
+        } else {
+            System.out.println(nome + " atacou (rolou " + rolar + ") → " + dano + " de dano em " + alvo.getNome());
+        }
+    }
+
+    public String getNome() { return nome; }
+    public int getVida() { return vida; }
+    public int getVidaMaxima() { return vidaMaxima; }
+    public boolean estaVivo() { return vida > 0; }
+
+    public void receberDano(int dano) {
+        vida = Math.max(0, vida - dano);
+    }
+
+    public void curar(int quantidade) {
+        vida = Math.min(vidaMaxima, vida + quantidade);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " [" + vida + "/" + vidaMaxima + " HP]";
+    }
+}
