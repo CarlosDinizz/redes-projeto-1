eb47bc455bc6d054e54bcf8f2fd2b596d3c2d7ad
Rafael Moutinho Tessarotto <94569688+RafaelTessarotto@users.noreply.github.com>
2026-04-09 11:13:34 -0300

    Implement Serializable and add tipo attribute

--- questao-3/Personagem.java
+++ questao-3/Personagem.java
@@ -1,19 +1,22 @@
 package jogo.personagens;
 
+import java.io.Serializable;
 import java.util.Random;
 
-public abstract class Personagem {
+public abstract class Personagem implements Serializable {
     protected String nome;
     protected int vida;
     protected int vidaMaxima;
     protected int ataque;
+    protected String tipo;
     private static final Random random = new Random();
 
-    public Personagem(String nome, int vida, int ataque) {
+    public Personagem(String nome, int vida, int ataque, String tipo) {
         this.nome = nome;
         this.vida = vida;
         this.vidaMaxima = vida;
         this.ataque = ataque;
+        this.tipo = tipo;
     }
 
     public void atacar(Personagem alvo) {
@@ -34,6 +37,8 @@ public abstract class Personagem {
     public int getVida() { return vida; }
     public int getVidaMaxima() { return vidaMaxima; }
     public boolean estaVivo() { return vida > 0; }
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
 
     public void receberDano(int dano) {
         vida = Math.max(0, vida - dano);
@@ -41,6 +46,10 @@ public abstract class Personagem {
 
     public void curar(int quantidade) {
         vida = Math.min(vidaMaxima, vida + quantidade);
+    }
+
+    public void curar() {
+        curar(5);
     }
 
     @Override
